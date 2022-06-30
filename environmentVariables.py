7fc2174934c85d3be34ead9b67e018b180400720
Fusilladin <fusilladin@protonmail.com>
2022-06-29 19:12:15 -0500

    Initial Commit
    Created environment variable and virtual enviroment studying material and notes for python

---
+++ environmentVariables.py
@@ -0,0 +1,15 @@
+import os
+
+db_user = os.environ['DB_USER']
+db_password = os.environ['DB_PASS']
+
+print(db_user)
+print(db_password)
+
+
+
+
+
+
+
+#
---
+++ venv_module.py
@@ -0,0 +1,27 @@
+import venv
+import os
+
+# move to folder
+cd desktop
+
+# name the virtual environment
+py -m venv [name]
+
+# activate v
+[env]\Scripts\activate.bat
+
+# Create a file
+type nul > example.py
+
+
+
+
+
+
+
+
+
+
+
+
+#
